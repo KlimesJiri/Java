2f164159fb9e20a8a8e09ef4a68b23551d4fab12
Jiří Klimeš <it.klimes@gmail.com>
2022-10-14 15:39:56 +0200

    Finished DataStructures Exercises

--- CoreJava/src/javabasics/data_structures/MapIntroduction2.java
+++ CoreJava/src/javabasics/data_structures/MapIntroduction2.java
@@ -5,27 +5,54 @@ import java.util.Map;
 
 public class MapIntroduction2 {
     public static void main(String[] args) {
+        //Create a map where the keys are strings and the values are strings with the following initial values
         Map<String, String> map2 = new HashMap<>();
-
+        //      Key    	            Value
+        // 978-1-60309-452-8 	A Letter to Jo
+        // 978-1-60309-459-7 	Lupus
+        // 978-1-60309-444-3 	Red Panda and Moon Bear
+        // 978-1-60309-461-0 	The Lab
         map2.put("978-1-60309-452-8", "A Letter to Jo");
         map2.put("978-1-60309-459-7", "Lupus");
         map2.put("978-1-60309-444-3", "Red Panda and Moon Bear");
         map2.put("978-1-60309-461-0", "The Lab");
 
+        //Print all the key-value pairs in the following format
+        // A Letter to Jo (ISBN: 978-1-60309-452-8)
+        // Lupus (ISBN: 978-1-60309-459-7)
+        // Red Panda and Moon Bear (ISBN: 978-1-60309-444-3)
+        // The Lab (ISBN: 978-1-60309-461-0)
         for (Map.Entry<String, String> entry : map2.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
             System.out.println(value + " (ISBN:" + key + ")");
         }
+
+        //Remove the key-value pair with key 978-1-60309-444-3
         map2.remove("978-1-60309-444-3");
 
+        //Remove the key-value pair with value The Lab
         map2.remove("978-1-60309-461-0", "The Lab");
 
+        //Add the following key-value pairs to the map
+        //  Key 	                Value
+        // 978-1-60309-450-4 	They Called Us Enemy
+        // 978-1-60309-453-5 	Why Did We Trust Him?
         map2.put("978-1-60309-450-4", "They Called Us Enemy");
         map2.put("978-1-60309-453-5", "Why Did We Trust Him?");
 
+        //Print whether there is an associated value with key 478-0-61159-424-8 or not
         System.out.println(map2.containsKey("478-0-61159-424-8"));
 
+        //Print the value associated with key 978-1-60309-453-5
         System.out.println(map2.get("978-1-60309-453-5"));
+
+        //The full output of your main method should be the following:
+        // A Letter to Jo (ISBN: 978-1-60309-452-8)
+        // Red Panda and Moon Bear (ISBN: 978-1-60309-444-3)
+        // Lupus (ISBN: 978-1-60309-459-7)
+        // The Lab (ISBN: 978-1-60309-461-0)
+        // false
+        // Why Did We Trust Him?
     }
 }
--- CoreJava/src/javabasics/data_structures/PersonalFinance.java
+++ CoreJava/src/javabasics/data_structures/PersonalFinance.java
@@ -5,9 +5,13 @@ import java.util.*;
 public class PersonalFinance {
     public static void main(String[] args) {
 
+        //Create a list with the following items:
+        //    500, 1000, 1250, 175, 800 and 120
         List<Integer> finance = new ArrayList<>(Arrays.asList(500, 1000, 1250, 175, 800, 120));
 
+        //Create an application which prints out the answers to the following questions:
 
+        //How much did we spend?
         System.out.println("How much did we spend?");
         int sum = 0;
         for (Integer pays : finance){
@@ -15,7 +19,7 @@ public class PersonalFinance {
         }
         System.out.println(sum);
 
-
+        //Which was our greatest expense?
         System.out.println("Which was our greatest expense?");
 //        int greatest = 0;
 //        for (Integer pays : finance){
@@ -26,14 +30,19 @@ public class PersonalFinance {
 //        System.out.println(greatest);
         System.out.println(Collections.max(finance));
 
-
+        //Which was our cheapest spending?
         System.out.println("Which was our cheapest spending?");
         System.out.println(Collections.min(finance));
 
-
+        //What was the average amount of our spendings? (print this as a float value)
         System.out.println("What was the average amount of our spendings? (print as a float");
         float average = Float.valueOf(sum) / finance.size();
         System.out.println(average);
 
+        //The full output of your main method should be the following:
+        // 3845
+        // 1250
+        // 120
+        // 640.8333
     }
 }
